6d8146068a212e6dee5a27bb8817b8c650e07f7f
Harmony02 <tadas.sin4@gmail.com>
2024-05-31 01:56:55 +0300

    durys random

--- Assets/Scripts/Interactables/DoorControl.cs
+++ Assets/Scripts/Interactables/DoorControl.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DoorControl : Interactable
@@ -7,10 +9,13 @@ public class DoorControl : Interactable
     [SerializeField]
     private GameObject door;
     private bool doorOpen;
+
+    private GameObject[] doors;
     // Start is called before the first frame update
     void Start()
     {
-        
+        doors = GameObject.FindGameObjectsWithTag("Door");
+        Debug.Log(doors.Length);
     }
 
     // Update is called once per frame
@@ -20,7 +25,17 @@ public class DoorControl : Interactable
     }
     protected override void Interact()
     {
-        doorOpen = !doorOpen;
+        foreach(GameObject door in doors)
+        {
+        int random = UnityEngine.Random.Range(0 , 2);
+        if(random==1)
+        doorOpen = true;
+        else 
+        doorOpen = false;
+        
         door.GetComponent<Animator>().SetBool("IsOpen", doorOpen);
+        Debug.Log(doorOpen);
+        }
     }
+
 }
